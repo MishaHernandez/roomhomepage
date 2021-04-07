e776498daf7e9fbdfe9a6796113377d66f567f29
Mijail Hernandez <mijailhernandez@gmail.com>
2021-04-07 11:38:58 -0500

    added slider.

---
+++ src/js/slider.js
@@ -0,0 +1,45 @@
+const sliderSection = document.getElementById("slider");
+let sliderContent = document.querySelectorAll(".slider-content");
+let sliderContentLast = sliderContent[sliderContent.length -1];
+const btnRight = document.getElementById("btnRight");
+const btnLeft = document.getElementById("btnLeft");
+
+sliderSection.insertAdjacentElement("afterbegin", sliderContentLast);
+
+function next() {
+    let sliderContentFirst = document.querySelectorAll(".slider-content")[0];
+    // sliderSection.classList.add(".js-slider-section");
+    sliderSection.style.marginLeft = "-200%";
+    sliderSection.style.transition = "all 0.5s";
+
+    setTimeout(() => {
+        sliderSection.style.transition = "none";
+        sliderSection.insertAdjacentElement("beforeend", sliderContentFirst);
+        sliderSection.style.marginLeft = "-100%";
+    }, 500);
+}
+
+function prev() {
+    let sliderContent = document.querySelectorAll(".slider-content");
+    let sliderContentLast = sliderContent[sliderContent.length -1];
+    sliderSection.style.marginLeft = "0";
+    sliderSection.style.transition = "all 0.5s";
+
+    setTimeout(() => {
+        sliderSection.style.transition = "none";
+        sliderSection.insertAdjacentElement("afterbegin", sliderContentLast);
+        sliderSection.style.marginLeft = "-100%";
+    }, 500);
+}
+
+btnRight.addEventListener("click", ()=> {
+    next();
+})
+
+btnLeft.addEventListener("click", () => {
+    prev();
+})
+
+// setInterval(() => {
+//     next();
+// }, 4000);
